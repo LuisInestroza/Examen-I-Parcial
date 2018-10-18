5e7c464c82254d0def8668438e8b66247f2aa1d8
Luis Dario Rodríguez <luisdario500@gmail.com>
2018-10-18 10:24:52 -0600

    Creación y  diseño del formulario

---
+++ ExamenPOO/ExamenPOO/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ExamenPOO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtMostarDatos = new System.Windows.Forms.TextBox();
+            this.txtComantarios = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValoracion = new System.Windows.Forms.TextBox();
+            this.txtCorreoElectronico = new System.Windows.Forms.TextBox();
+            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbNombreCompleto = new System.Windows.Forms.Label();
+            this.llbCorreroElectronico = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Controls.Add(this.txtMostarDatos);
+            this.groupBox1.Controls.Add(this.txtComantarios);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtValoracion);
+            this.groupBox1.Controls.Add(this.txtCorreoElectronico);
+            this.groupBox1.Controls.Add(this.txtNombreCompleto);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbNombreCompleto);
+            this.groupBox1.Controls.Add(this.llbCorreroElectronico);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 365);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione el producto a realizar su reseña/análisis";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(298, 320);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(138, 28);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(122, 320);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(138, 28);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // txtMostarDatos
+            // 
+            this.txtMostarDatos.Location = new System.Drawing.Point(15, 29);
+            this.txtMostarDatos.Multiline = true;
+            this.txtMostarDatos.Name = "txtMostarDatos";
+            this.txtMostarDatos.Size = new System.Drawing.Size(210, 271);
+            this.txtMostarDatos.TabIndex = 9;
+            // 
+            // txtComantarios
+            // 
+            this.txtComantarios.Location = new System.Drawing.Point(268, 198);
+            this.txtComantarios.Multiline = true;
+            this.txtComantarios.Name = "txtComantarios";
+            this.txtComantarios.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtComantarios.Size = new System.Drawing.Size(275, 102);
+            this.txtComantarios.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Comentarios";
+            // 
+            // txtValoracion
+            // 
+            this.txtValoracion.Location = new System.Drawing.Point(269, 150);
+            this.txtValoracion.Name = "txtValoracion";
+            this.txtValoracion.Size = new System.Drawing.Size(274, 24);
+            this.txtValoracion.TabIndex = 7;
+            // 
+            // txtCorreoElectronico
+            // 
+            this.txtCorreoElectronico.Location = new System.Drawing.Point(269, 102);
+            this.txtCorreoElectronico.Name = "txtCorreoElectronico";
+            this.txtCorreoElectronico.Size = new System.Drawing.Size(274, 24);
+            this.txtCorreoElectronico.TabIndex = 6;
+            // 
+            // txtNombreCompleto
+            // 
+            this.txtNombreCompleto.Location = new System.Drawing.Point(268, 54);
+            this.txtNombreCompleto.Name = "txtNombreCompleto";
+            this.txtNombreCompleto.Size = new System.Drawing.Size(275, 24);
+            this.txtNombreCompleto.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valoración";
+            // 
+            // lbNombreCompleto
+            // 
+            this.lbNombreCompleto.AutoSize = true;
+            this.lbNombreCompleto.Location = new System.Drawing.Point(265, 29);
+            this.lbNombreCompleto.Name = "lbNombreCompleto";
+            this.lbNombreCompleto.Size = new System.Drawing.Size(135, 18);
+            this.lbNombreCompleto.TabIndex = 1;
+            this.lbNombreCompleto.Text = "Nombre Completo:";
+            // 
+            // llbCorreroElectronico
+            // 
+            this.llbCorreroElectronico.AutoSize = true;
+            this.llbCorreroElectronico.Location = new System.Drawing.Point(266, 81);
+            this.llbCorreroElectronico.Name = "llbCorreroElectronico";
+            this.llbCorreroElectronico.Size = new System.Drawing.Size(134, 18);
+            this.llbCorreroElectronico.TabIndex = 2;
+            this.llbCorreroElectronico.Text = "Correo Electrónico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 393);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Reseña/Análisi de Productos - AdventureWorks";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txtMostarDatos;
+        private System.Windows.Forms.TextBox txtComantarios;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValoracion;
+        private System.Windows.Forms.TextBox txtCorreoElectronico;
+        private System.Windows.Forms.TextBox txtNombreCompleto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbNombreCompleto;
+        private System.Windows.Forms.Label llbCorreroElectronico;
+    }
+}
+
